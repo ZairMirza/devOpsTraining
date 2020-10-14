51e923e3ec66a9f3a9cec6ab8a2ce0a4f690a720
UzairMirza <uzair.mirza44@gmail.com>
2020-10-14 11:51:30 +0500

    Hello World Commit

---
+++ src/test/java/devOpsTraining/HelloWorld.java
@@ -0,0 +1,10 @@
+package devOpsTraining;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
